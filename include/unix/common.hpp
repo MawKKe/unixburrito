fc3ada72e8122f6d85a23a819613dc5500c1dd94
Markus (MawKKe) <ekkwam@gmail.com>
2018-12-10 18:27:38 +0200

    Fixed errno_str (strerror_r wrapper)
    Stupid platform dependent implementations. Why the fuck did you crete
    differently behaving function with the same name!?!?!!

--- include/unix/common.hpp
+++ include/unix/common.hpp
@@ -3,12 +3,23 @@
 #include <string>
 #include <cstring>
 
+// Compiler should select the corret overload depending on your ::strerror_r variant
+// GNU version
+static inline std::string __strerror_r(const char* ret, const char * buf, int e) {
+	(void)buf;
+	return (ret == nullptr) ? ("Unknown error code: " + std::to_string(e)) : std::string(ret);
+}
+// XSI version
+static inline std::string __strerror_r(int ret, const char * buf, int e){
+	return (ret == 0) ? std::string((const char*)buf) : ("Unknown error code: " + std::to_string(e));
+}
+
 namespace unix {
-    // basically wrapper around strerror_r
+
+	// Actual interface
     inline std::string errno_str(int e){
-        char buf[256];
-		// This is how big boys get around stupid compliance issues (XSI vs GNU) :D
-        auto ret = ::strerror_r(e, buf, sizeof(buf));
-        return (!ret) ? ("Unknown error code: " + std::to_string(e)) : std::string(buf);
+        char buf[256] = {};
+        return __strerror_r(::strerror_r(e, buf, sizeof(buf)), buf, e);
+
     }
 }
