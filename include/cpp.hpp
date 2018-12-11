79376aedce5e7982bf7a62bc9d1501342c691d4a
Markus (MawKKe) <ekkwam@gmail.com>
2018-12-11 15:59:08 +0200

    Add cool EnumCheck template monster

--- include/cpp.hpp
+++ include/cpp.hpp
@@ -46,6 +46,31 @@ namespace cpp {
 	constexpr int to_int(const std::initializer_list<T> & l){
 		return to_int(std::begin(l), std::end(l));
 	}
+
+    template<typename EnumType, EnumType... Values>
+    class EnumCheck;
+
+    template<typename EnumType>
+    class EnumCheck<EnumType>
+    {
+    public:
+        template<typename IntType>
+        static bool constexpr is_value(IntType) { return false; }
+    };
+
+    template<typename EnumType, EnumType V, EnumType... Next>
+    class EnumCheck<EnumType, V, Next...> : private EnumCheck<EnumType, Next...>
+    {
+        using super = EnumCheck<EnumType, Next...>;
+
+    public:
+        template<typename IntType>
+        static bool constexpr is_value(IntType v)
+        {
+            return v == static_cast<IntType>(V) || super::is_value(v);
+        }
+    };
+
 }
 
 template <typename T>
