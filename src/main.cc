a5d1188735744e103b9869725adaa8182947a580
Markus (MawKKe) <ekkwam@gmail.com>
2019-01-07 15:30:49 +0200

    Modify main.cc debug prints

--- src/main.cc
+++ src/main.cc
@@ -47,6 +47,7 @@ void handle_in(unix::inet::Socket & s){
             std::cerr << "omg sendto has problems, gosh: " << std::to_string(n2) << "\n";
         }
     }
+    std::cerr << "---\n";
 
 }
 
@@ -87,7 +88,7 @@ int server(int argc, const char* argv[]){
     using namespace std::chrono_literals;
 
     while(run){
-        std::cerr << "DEBUG: waiting..\n";
+        //std::cerr << "DEBUG: waiting..\n";
         EventList<10> evts;
         auto n_ev = epoll.wait(evts, 500ms);
         if(n_ev < 0){
@@ -95,7 +96,9 @@ int server(int argc, const char* argv[]){
             std::cerr << "ERROR - Epoll::wait(): " << unix::errno_str(errno) << std::endl;;
             break;
         }
-        std::cerr << "epoll_wait returned: " << n_ev << std::endl;
+        if(n_ev > 0){
+            std::cerr << "epoll_wait returned: " << n_ev << std::endl;
+        }
         for(int i = 0; i < n_ev; ++i){
             if(evts[i].matches(s) && (evts[i] & EpollEventType::Input)){
                 handle_in(s);
